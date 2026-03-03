649c1de02f052f256b201516c6ca65112a513b35
MatthewPage1 <matthew.page1000@gmail.com>
2026-03-03 21:22:12 +0000

    Barcode + itemlist popup working
    Items can now be pressed and a deeper rundown of the product information can be viewed

--- MP-Project.Shared/Product.cs
+++ MP-Project.Shared/Product.cs
@@ -4,11 +4,18 @@
 	{
 		public int ProductId { get; set; }
 		public string? ProductName { get; set; }
+		public string? gtin13 { get; set; }
 		public int StockCount { get; set; }
 		public int ReorderLevel { get; set; }
 		public string? Availability { get; set; }
 		public string? ProductDescription { get; set; }
 		public string? ProductGroup1 { get; set; }
+		public string? ProductGroup2 { get; set; }
+		public string? ProductGroup3 { get; set; }
+		public string? Brand { get; set; }
+
+		public decimal CostPrice { get; set; }
+		public decimal SellingPrice { get; set; }
 		public string? Image { get; set; }
 	}
 }
